b9b8b4cc9490d8f281c0f1b28f855db3e2a0d3a9
John Eubanks <jeubanks@simplextech.net>
2019-12-10 09:27:41 -0500

    Added utility processor

---
+++ utils.py
@@ -0,0 +1,11 @@
+
+
+
+def meters_to_feet(meters):
+    ft = round((meters / 1000) * 3.2808, 1)
+    return ft
+
+
+def kilogram_to_pound(kilogram):
+    lb = round((kilogram / 1000) * 2.2046, 2)
+    return lb
